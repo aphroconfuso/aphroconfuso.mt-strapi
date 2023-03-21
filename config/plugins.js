837d04b31e0a1bd73ca0b24c02326e2182978e79
Joe Gatt <joe@joegatt.net>
2023-03-21 01:17:46 +0100

    Add TinyMCE

---
+++ config/plugins.js
@@ -0,0 +1,50 @@
+module.exports = ({env}) => ({
+	tinymce: {
+			enabled: true,
+			config: {
+					editor: {
+							outputFormat: "html",
+							editorConfig: {
+									language: "en",
+									height: 500,
+									menubar: false,
+									extended_valid_elements: "span, img, small",
+									forced_root_block: "",
+									convert_urls: false,
+									entity_encoding: "raw",
+									plugins:
+											"advlist autolink lists link image charmap preview anchor \
+											searchreplace visualblocks code fullscreen table emoticons nonbreaking \
+											insertdatetime media table code help wordcount",
+									toolbar:
+											"undo redo | styles | bold italic | \
+											alignleft aligncenter alignright | \
+											table visualblocks code|\
+											nonbreaking bullist numlist | removeformat",
+									style_formats: [
+											{
+													title: "Headings",
+													items: [
+															{ title: "h4", block: "h4" },
+															{ title: "h5", block: "h5" },
+															{ title: "h6", block: "h6" },
+													],
+											},
+
+											{
+													title: "Text",
+													items: [
+															{ title: "Paragraph", block: "p" },
+															{
+																	title: "Paragraph with small letters",
+																	block: "small",
+															},
+													],
+											},
+								],
+									content_css: '/tinymce.css',
+							},
+					},
+			},
+	},
+});
