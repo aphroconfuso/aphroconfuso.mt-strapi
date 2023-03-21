537549f293c99bfb69305652efbfcb7d7863e580
Joe Gatt <joe@joegatt.net>
2023-03-22 00:23:26 +0100

    Fixed dev

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,13 +1,11 @@
-module.exports =  ({ env }) => ({
-	connection: {
-		client: 'postgres',
-		connection: {
-		host: env('DATABASE_HOST', 'localhost'),
-			port: env.int('DATABASE_PORT', 5437),
-			database: env('DATABASE_NAME', 'aphroconfusodb'),
-			user: env('DATABASE_USERNAME', 'strapi'),
-			password: env('DATABASE_PASSWORD', 'dfgg34535jdfgoiuuyyyTTREEasdfaskdnkasjd'),
-			ssl: env.bool('DATABASE_SSL', false)
-		}
-	}
+const path = require('path');
+
+module.exports = ({env}) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
+  },
 });
