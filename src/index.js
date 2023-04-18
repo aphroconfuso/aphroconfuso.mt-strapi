0b749a3b4e80758d10207fc80759569853f55aab
Joe Gatt <joe@joegatt.net>
2023-04-18 21:06:06 +0200

    Remove auth example for now

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 'use strict';
 
-module.exports = {
-  register({ strapi }) {
-    const extensionService = strapi.plugin('graphql').service('extension');
-
-    extensionService.use({
-      resolversConfig: {
-        'Query.abouts': {
-          auth: false
-        },
-        'Query.stories': {
-          auth: {
-            scope: ['api::stories.title.find']
-          }
-        },
-      }
-    })
-  }
-}
-
 // module.exports = {
-//   /**
-//    * An asynchronous register function that runs before
-//    * your application is initialized.
-//    *
-//    * This gives you an opportunity to extend code.
-//    */
-//   register(/*{ strapi }*/) {},
+//   register({ strapi }) {
+//     const extensionService = strapi.plugin('graphql').service('extension');
 
-//   /**
-//    * An asynchronous bootstrap function that runs before
-//    * your application gets started.
-//    *
-//    * This gives you an opportunity to set up your data model,
-//    * run jobs, or perform some special logic.
-//    */
-//   bootstrap(/*{ strapi }*/) {},
-// };
+//     extensionService.use({
+//       resolversConfig: {
+//         'Query.abouts': {
+//           auth: false
+//         },
+//         'Query.stories': {
+//           auth: {
+//             scope: ['api::stories.title.find']
+//           }
+//         },
+//       }
+//     })
+//   }
+// }
+
+module.exports = {
+  /**
+   * An asynchronous register function that runs before
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
+  register(/*{ strapi }*/) {},
+
+  /**
+   * An asynchronous bootstrap function that runs before
+   * your application gets started.
+   *
+   * This gives you an opportunity to set up your data model,
+   * run jobs, or perform some special logic.
+   */
+  bootstrap(/*{ strapi }*/) {},
+};
