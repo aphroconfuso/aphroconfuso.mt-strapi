35c0a3ed71fa3c9795c793a0e7729051eb9c048c
Joe Gatt <joe@joegatt.net>
2023-04-22 21:27:43 +0200

    Update plugins and schemas

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -51,8 +51,8 @@ module.exports = ({env}) => ({
 							editorConfig: {
 									language: "en",
 									height: 500,
-								menubar: false,
-									valid_elements: "p[class],em,strong,h4,h5,h6,ul,ol,li,code,img,figure,figcaption",
+									menubar: false,
+									valid_elements: "a,blockquote,p[class],em,strong,h4,h5,h6,ul,ol,li,code,img,figure,figcaption",
 									forced_root_block: "",
 									convert_urls: false,
 									entity_encoding: "raw",
@@ -83,6 +83,7 @@ module.exports = ({env}) => ({
 																block: "p",
 																classes: "enjambed"
 															},
+															{ title: "Blockquote", block: "blockquote" },
 													],
 											},
 								],
